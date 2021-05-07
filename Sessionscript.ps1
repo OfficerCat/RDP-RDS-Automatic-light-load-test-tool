8047b7b4e1b27fec57f0122796e9bba3bb9b7e8a
OfficerCat <jo.filler1998@gmail.com>
2021-05-07 16:04:22 +0200

    Added Check if user exists or not

--- Sessionscript.ps1
+++ Sessionscript.ps1
@@ -1,10 +1,22 @@
 # Zufällig einen User aus der User.txt auswählen
-$user = gc C:\Powershell_Auto\User.txt | sort{get-random} | select -First 1
-$Password = "Katzenfutter"
+$user = gc User.txt | sort{get-random} | select -First 1
+do {
+    $userexists = Select-String -Path inuse.txt -Pattern $user -SimpleMatch
+    If ($userexists.length -gt 0) 
+    {
+        $user = gc User.txt | sort{get-random} | select -First 1
+        Write-Host 'Error'        
+    }
+}
+while ($userexists.length -gt 0)
+Add-Content -Path inuse.txt -Value $user
+$Password = "Xfdsad!!!"
 $RDShost = "159.69.229.235"
 Write-Host $user
 
 cmdkey /generic:TERMSRV/$RDShost /user:$user /pass:$Password
 mstsc /v:$RDShost
 
-Invoke-Command -Computername $RDShost -Credential Administrator -ScriptBlock {."c:\Scripts\script.ps1"} #Assuming the script is on the C:\ Drive of the RemoteCompute
+Invoke-Command -Computername $RDShost -Credential Administrator -ScriptBlock {."c:\Scripts\script.ps1"} #Assuming the script is on the C:\ Drive of the RemoteComputer
+
+(Get-Content inuse.txt).replace($user, '') | Set-Content inuse.txt#
