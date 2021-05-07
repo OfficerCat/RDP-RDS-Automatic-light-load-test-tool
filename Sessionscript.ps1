b8cd32fee0cd8b3d9112a8f41a6132323c16de8c
OfficerCat <jo.filler1998@gmail.com>
2021-05-07 16:50:21 +0200

    Added Question about PW and RDS Host

--- Sessionscript.ps1
+++ Sessionscript.ps1
@@ -1,22 +1,22 @@
 # Zufällig einen User aus der User.txt auswählen
-$user = gc User.txt | sort{get-random} | select -First 1
+$RDShost = Read-Host -Prompt 'RDS Host IP or FQDN'
+$Password = Read-Host -Prompt 'Input Default Password for all Users'
+$user = gc user.txt | sort{get-random} | select -First 1
 do {
     $userexists = Select-String -Path inuse.txt -Pattern $user -SimpleMatch
     If ($userexists.length -gt 0) 
     {
-        $user = gc User.txt | sort{get-random} | select -First 1
+        $user = gc user.txt | sort{get-random} | select -First 1
         Write-Host 'Error'        
     }
 }
 while ($userexists.length -gt 0)
 Add-Content -Path inuse.txt -Value $user
-$Password = "Xfdsad!!!"
-$RDShost = "159.69.229.235"
 Write-Host $user
 
 cmdkey /generic:TERMSRV/$RDShost /user:$user /pass:$Password
-mstsc /v:$RDShost
+#mstsc /v:$RDShost
 
-Invoke-Command -Computername $RDShost -Credential Administrator -ScriptBlock {."c:\Scripts\script.ps1"} #Assuming the script is on the C:\ Drive of the RemoteComputer
+#Invoke-Command -Computername $RDShost -Credential Administrator -ScriptBlock {."c:\Scripts\script.ps1"} #Assuming the script is on the C:\ Drive of the RemoteComputer
 
-(Get-Content inuse.txt).replace($user, '') | Set-Content inuse.txt#
+(Get-Content inuse.txt).replace($user, '') | Set-Content inuse.txt
