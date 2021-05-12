f9b89e921990d0491e3854343d99db09ad16f061
OfficerCat <jo.filler1998@gmail.com>
2021-05-12 08:53:29 +0200

    Prompt for IP FQDN and Passwort Preperation for Destination Prompt

--- Sessionscript.ps1
+++ Sessionscript.ps1
@@ -1,6 +1,8 @@
-# Zufällig einen User aus der User.txt auswählen
+#Daten Abfragen
+#$Remotedestination = Read-Host -Prompt "Full Path of the Bench.ps1 skriptfile on the Remotehost example: C:\Bench.ps1"
 $RDShost = Read-Host -Prompt 'RDS Host IP or FQDN'
 $Password = Read-Host -Prompt 'Input Default Password for all Users'
+# Zufällig einen User aus der User.txt auswählen
 $user = gc user.txt | sort{get-random} | select -First 1
 do {
     $userexists = Select-String -Path inuse.txt -Pattern $user -SimpleMatch
@@ -17,6 +19,8 @@ Write-Host $user
 cmdkey /generic:TERMSRV/$RDShost /user:$user /pass:$Password
 #mstsc /v:$RDShost
 
-#Invoke-Command -Computername $RDShost -Credential Administrator -ScriptBlock {."c:\Scripts\script.ps1"} #Assuming the script is on the C:\ Drive of the RemoteComputer
+Invoke-Command -Computername $RDShost -Credential $user -ScriptBlock {."C:\Bench.ps1"} #Assuming the script is on the C:\ Drive of the RemoteComputer
 
 (Get-Content inuse.txt).replace($user, '') | Set-Content inuse.txt
+
+#set-item wsman:localhost\client\trustedhosts -value *
