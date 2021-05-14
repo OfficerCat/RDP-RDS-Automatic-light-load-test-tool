d1e2039fa89625f7f2e49f3725a9ee0001a63fa7
OfficerCat <jo.filler1998@gmail.com>
2021-05-14 11:56:51 +0200

    0

--- SessionID.ps1
+++ SessionID.ps1
@@ -1,10 +1,1 @@
 $users = qwinsta Administrator ;
-$IDs = @() ;
-
-for ($i=1; $i -lt $users.Count;$i++) {
-  # using regex to find the IDs
-  $temp = [string]($users[$i] | Select-String -pattern "\s\d+\s").Matches ;
-  $temp = $temp.Trim() ;
-  $IDs += $temp ;
-}
-return $IDs
--- Sessionscript.ps1
+++ Sessionscript.ps1
@@ -1,4 +1,4 @@
-#Daten Abfragen
+
 #$Remotedestination = Read-Host -Prompt "Full Path of the Bench.ps1 skriptfile on the Remotehost example: C:\Bench.ps1"
 $RDShost = Read-Host -Prompt 'RDS Host IP or FQDN'
 $Password = Read-Host -Prompt 'Input Default Password for all Users'
@@ -14,13 +14,27 @@ do {
 }
 while ($userexists.length -gt 0)
 Add-Content -Path inuse.txt -Value $user
+#
 Write-Host $user
-
+#Starte RDP Session
 cmdkey /generic:TERMSRV/$RDShost /user:$user /pass:$Password
-#mstsc /v:$RDShost
+mstsc /v:$RDShost
 
-Invoke-Command -Computername $RDShost -Credential $user -ScriptBlock {."C:\Bench.ps1"} #Assuming the script is on the C:\ Drive of the RemoteComputer
+#RDP Session ID
+$users = Invoke-command –computername $RDShost -credential $user –scriptblock {c:\SessionID.ps1}
+$IDs = @() ;
 
+for ($i=1; $i -lt $users.Count;$i++) {
+  #using regex to find the IDs
+  $temp = [string]($users[$i] | Select-String -pattern ".*$user.* \d+\s").Matches ;
+  $tempID = [string]($temp | Select-String -pattern "\s\d+\s").Matches ;
+  $tempID = $tempID.Trim() ;
+  $IDs += $tempID ;
+}
+$IDs
+
+
+#Remove from use list
 (Get-Content inuse.txt).replace($user, '') | Set-Content inuse.txt
 
 #set-item wsman:localhost\client\trustedhosts -value *
