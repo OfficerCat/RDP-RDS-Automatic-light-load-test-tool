be52a94b3cac41c403e2f60cfb5041f1a02c3730
OfficerCat <jo.filler1998@gmail.com>
2021-09-23 16:23:10 +0200

    fixed RegEX for ID

--- Sessionscript.ps1
+++ Sessionscript.ps1
@@ -13,25 +13,25 @@ $securePassword = ConvertTo-SecureString $AdminPassword -AsPlainText -force
 $credential = New-Object System.Management.Automation.PsCredential($Adminuser, $securePassword)
 
 do {
-  $userexists = Select-String -Path inuse.txt -Pattern $user -SimpleMatch
+  $userexists = Get-ChildItem -Path TMP -Name "$user" 
   If ($userexists.length -gt 0) {
     $user = Get-Content user.txt | Sort-Object { get-random } | Select-Object -First 1
     Write-Host 'Error'        
   }
 }
 while ($userexists.length -gt 0)
-Add-Content -Path inuse.txt -Value $user
+New-Item -Path TMP -Name $user
 Write-Host $user
 #Starting RDP Session
 #DOMÄNE!!!
 cmdkey /generic:TERMSRV/$RDShost /user:RDS\$user /pass:$Password
 mstsc /w:800 /h:600 /v:$RDShost
-Start-Sleep -Seconds 10Select-
+Start-Sleep -Seconds 10
 #RDP Session ID
 $users = Invoke-command $RDShost -credential $credential -scriptblock { c:\PSTest\SessionID.ps1 $user }
 $IDs = @() ;
 for ($i = 1; $i -lt $users.Count; $i++) {
-  #using regex to find the IDs
+  #using regex to find the ID
   $temp = [string]($users[$i] | Select-String -pattern .*$user.*\d+\s).Matches;
   $tempID = [string]($temp | Select-String -pattern  \s\d+\s ).Matches;
   $tempID = $tempID.Trim() ;
@@ -41,5 +41,4 @@ $IDs
 Psexec.exe \\$RDShost -i $IDs -u $user -p $Password powershell "C:\PSTest\Bench.ps1"
 #Remove from inuse.txt list
 Psexec.exe \\$RDShost -i $IDs -u $user -p $Password powershell "shutdown /l /f"
-Start-Sleep -s 8
-Set-Content -Path "inuse.txt" -Value (get-content -Path "inuse.txt" | Select-String -Pattern "$user$" -NotMatch)
+Remove-Item TMP/$user -Force -Confirm:$false
