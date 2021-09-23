af1d4667e1ea22b8bcab9fc0070b5f3e2832c4ec
OfficerCat <jo.filler1998@gmail.com>
2021-09-23 09:58:08 +0200

    Changed Benchmark due to higher demand needed to benchmark servers Changed "logout" invoker to RDP Host to avoid timeout

--- Runme.ps1
+++ Runme.ps1
@@ -30,13 +30,13 @@ do {
             Start-Process -windowstyle hidden -Filepath powershell.exe -WorkingDirectory C:\Users\Administrator\Desktop\RDSScript -ArgumentList "C:\Users\Administrator\Desktop\RDSScript\Sessionscript.ps1 -Adminuser $Adminuser -Adminpassword $Adminpassword -RDShost $RDShost -Password $Password"
             $Check = Get-WmiObject Win32_Process -Filter "Name='powershell.exe' AND CommandLine LIKE '%Sessionscript.ps1%'"
             $temp = $Check | Select-String  -Pattern "Win32"
-            Start-Sleep 2
+            Start-Sleep 1
             $CPUUsageRDS = Invoke-command $RDShost -credential $credential -scriptblock {Get-WmiObject Win32_Processor | Select-Object LoadPercentage | Format-List | Out-String }           
             clear
             Write-Host "RDS Host CPU Usage: $CPUUsageRDS"
             Write-Host "Client CPU Usage $CPUUsageClient"
             Write-Host "Cooldown 2 seconds"
-            Start-Sleep 2
+            Start-Sleep 1
             
         } while ($temp.count -lt $Sessioncount) 
     }
@@ -47,7 +47,7 @@ do {
         Write-Host "RDS Host CPU Usage: $CPUUsageRDS"
         Write-Host "Client CPU Usage $CPUUsageClient"
         Write-Host "Max Sessions reached, waiting"
-        Start-Sleep 2       
+        Start-Sleep 1       
     }
     
 } while ($continue -eq "true")
